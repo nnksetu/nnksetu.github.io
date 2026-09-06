0bf947e363afc7d273ce0273222ba00ce2e84a75
Aomul <44919078+Aomul@users.noreply.github.com>
2026-09-06 12:32:43 +0800

    1

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,5 +1,5 @@
 const DEFAULT_WORKER_DOWNLOAD_DOMAIN = "https://dl.setutime.top";
-const DEFAULT_VIDEO_MEDIA_DOMAIN = "https://eo.setutime.top";
+const DEFAULT_VIDEO_MEDIA_DOMAIN = "https://v.setutime.top";
 const DEFAULT_IMAGE_MEDIA_DOMAIN = "https://r2.setutime.top";
 
 function getDomainSetting(env, name, fallback) {
